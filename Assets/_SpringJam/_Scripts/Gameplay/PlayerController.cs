a0dcfac07ac800383ab8ff43338b12c6a4eace5b
Ylona Meeuwenberg <meeuwenberg.ylona@gmail.com>
2026-04-04 13:12:12 +0200

    Basic movement
    - Added a free asset pack
    - Basic movement (Arrows + WASD)
    - Camara setup 2.5d

---
+++ Assets/_SpringJam/_Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private int speed;
+
+    private InputSystem_Actions playerControlls;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    void Awake()
+    {
+        playerControlls = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        playerControlls.Enable();
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = playerControlls.Player.Move.ReadValue<Vector2>().x;
+        float z = playerControlls.Player.Move.ReadValue<Vector2>().y;
+
+        Debug.Log(x + " , " + z);
+
+        movement = new Vector3(x, 0, z).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
