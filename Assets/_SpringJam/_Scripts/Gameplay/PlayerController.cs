5a94833aec8d2597e205b024ba3039f73d532c54
Ylona Meeuwenberg <meeuwenberg.ylona@gmail.com>
2026-04-17 17:35:55 +0200

    Player Animations

--- Assets/_SpringJam/_Scripts/Gameplay/PlayerController.cs
+++ Assets/_SpringJam/_Scripts/Gameplay/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour, ILoopResetListener
 
     private PlayerInputHandler input;
     private Rigidbody rb;
+    private Animator animator;
+
     private Vector3 movement;
+
+    private static readonly int MoveX = Animator.StringToHash("MoveX");
+    private static readonly int MoveY = Animator.StringToHash("MoveY");
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
 
     private void Awake()
     {
@@ -18,7 +24,25 @@ public class PlayerController : MonoBehaviour, ILoopResetListener
         }
 
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+
     }
+
+
+    private void Update()
+    {
+        if (animator == null) return;
+
+        bool moving = movement.sqrMagnitude > 0f;
+        animator.SetBool(IsMoving, moving);
+
+        if (moving)
+        {
+            animator.SetFloat(MoveX, movement.x);
+            animator.SetFloat(MoveY, movement.z);
+        }
+    }
+
 
     private void FixedUpdate()
     {
