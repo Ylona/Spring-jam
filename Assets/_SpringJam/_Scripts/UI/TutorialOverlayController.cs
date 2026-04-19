cd20615a2521ff642d3a19b4a4bf2b91671f1c35
Ylona Meeuwenberg <meeuwenberg.ylona@gmail.com>
2026-04-19 13:21:46 +0200

    Tutorial start screen

---
+++ Assets/_SpringJam/_Scripts/UI/TutorialOverlayController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TutorialOverlayController : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+
+    private VisualElement root;
+    private Button closeButton;
+
+    private void OnEnable()
+    {
+        root = uiDocument.rootVisualElement;
+        root.pickingMode = PickingMode.Position;
+
+        closeButton = root.Q<Button>("CloseButton");
+        if (closeButton != null)
+        {
+            closeButton.clicked -= Hide;
+            closeButton.clicked += Hide;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (closeButton != null)
+            closeButton.clicked -= Hide;
+    }
+
+    public void Show()
+    {
+        if (root != null)
+            root.style.display = DisplayStyle.Flex;
+    }
+
+    public void Hide()
+    {
+        if (root != null)
+            root.style.display = DisplayStyle.None;
+    }
+}
